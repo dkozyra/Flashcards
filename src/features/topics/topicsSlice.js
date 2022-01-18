15025f638466830f76ff5e698f2704e6d90ab32c
Damian Kozyra <d.kozyra@hotmail.co.uk>
2022-01-18 17:28:57 +0000

    complete topicSlice

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const topicsSlice = createSlice({
+    name: "topics",
+    initialState: {topics: {}},
+    reducers: {
+      addTopic: (state, action) => {
+        const newTopic = { 
+            id: action.payload.id,
+            name: action.payload.name,
+            icon: action.payload.icon,
+            quizIds: []
+        }
+        state.topics[action.payload.id] = newTopic;
+      }
+    }
+});
+
+export const selectTopics = state => state.topics.topics;
+  
+export const addTopic = topicsSlice.actions.addTopic;
+ 
+export default topicsSlice.reducer;
